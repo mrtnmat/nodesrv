a2c4d90663913c00e4e0e888ce69a643a0ff5c80
Matteo <mrtn.matteo@protonmail.com>
2022-10-03 12:59:33 +0200

    split db query and server

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const sqlite3 = require('sqlite3').verbose();
-const http = require('http');
-const ejs = require('ejs');
-var fs = require('fs'); 
+const sqlite3 = require('sqlite3').verbose()
+const http = require('http')
+const ejs = require('ejs')
+var fs = require('fs')
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'
 const port = 3000;
-const dbpath = './db/test.db';
+const dbpath = './db/test.db'
 const db = new sqlite3.Database(dbpath, (err) => {
   if (err) {
     console.error(err.message)
@@ -15,24 +15,27 @@ const db = new sqlite3.Database(dbpath, (err) => {
   console.log(`Connected to ${dbpath}`)
 })
 
-const server = http.createServer((req, res) => {
-  let sql = `SELECT DISTINCT Name name FROM test
-  ORDER BY name`;
-  
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    const r = []
-    rows.forEach((e) => {
-      r.push(e.name)
-    })
-    const htmlContent = fs.readFileSync(__dirname + '/index.ejs', 'utf8');
-    const htmlRenderized = ejs.render(htmlContent, {filename: 'index.ejs', r: r});  
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'html');
-    res.end(htmlRenderized)
+const r = []
+
+let sql = `SELECT DISTINCT Name name FROM test
+  ORDER BY name`
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err
+  }
+  rows.forEach((e) => {
+    r.push(e.name)
   })
+})
+
+const server = http.createServer((req, res) => {
+    const htmlContent = fs.readFileSync(__dirname + '/index.ejs', 'utf8')
+    const htmlRenderized = ejs.render(htmlContent, {filename: 'index.ejs', r: r})
+    console.log('prova')
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'html')
+    res.end(htmlRenderized)
 });
 
 server.listen(port, hostname, () => {
