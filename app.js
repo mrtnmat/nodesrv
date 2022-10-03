edc90688f1065adbffc6544428549d3b48d43e21
Matteo <mrtn.matteo@protonmail.com>
2022-10-03 07:12:31 +0200

    ejs templating the db query

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 const sqlite3 = require('sqlite3').verbose();
 const http = require('http');
+const ejs = require('ejs');
+var fs = require('fs'); 
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const db = new sqlite3.Database('./db/test.db', (err) => {
-    if (err) {
-        console.error(err.message)
-    }
-    console.log('Connected to the test database')
-})
-
-var rows = [];
-
-db.serialize(() => {
-    db.each(`SELECT Id as id,
-                    Name as name
-             FROM test`, (err, row) => {
-        if (err) {
-            console.error(err.message)
-        }
-        console.log(row.id + "\t" + row.name);
-        rows.push(row.name);
-    })
+const dbpath = './db/test.db';
+const db = new sqlite3.Database(dbpath, (err) => {
+  if (err) {
+    console.error(err.message)
+  }
+  console.log(`Connected to ${dbpath}`)
 })
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  let r = "Database Query Result:\n"
-  rows.forEach((entry) => {
-    r += entry + '\n'
+  let sql = `SELECT DISTINCT Name name FROM test
+  ORDER BY name`;
+  
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    const r = []
+    rows.forEach((e) => {
+      r.push(e.name)
+    })
+    const htmlContent = fs.readFileSync(__dirname + '/index.ejs', 'utf8');
+    const htmlRenderized = ejs.render(htmlContent, {filename: 'index.ejs', r: r});  
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'html');
+    res.end(htmlRenderized)
   })
-  res.end(r);
 });
 
 server.listen(port, hostname, () => {
